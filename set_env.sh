5d90846e241c167da2f9253a043b919599c57d81
Guilherme Uezima <guilherme@uezima.com>
2020-09-13 16:38:10 -0300

    chore: git config

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME="udagramuezimadev"
+export POSTGRES_PASSWORD="udagramuezimadev"
+export POSTGRES_HOST="udagramuezimadev.cdzjdlyl6c6w.us-east-2.rds.amazonaws.com"
+export POSTGRES_DB="udagramuezimadev"
+export AWS_BUCKET="udagram-uezima-dev"
+export AWS_REGION="us-east-2"
+export AWS_PROFILE="default"
+export JWT_SECRET="testejwtsecret"
+export URL=http://localhost:8081
+export PORT=8081
